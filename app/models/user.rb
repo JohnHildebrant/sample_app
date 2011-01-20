6b9ebb6fecb39dc8436613119067a53b1511a79d
John Hildebrant <john.hildebrant@wizards.com>
2011-01-19 17:09:48 -0800

    Done with user edit/update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110117175055
+# Schema version: 20110120004058
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
